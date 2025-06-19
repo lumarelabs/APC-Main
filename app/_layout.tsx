04b114fe89bd5b09c54d3acbb57b3a4226912531
lumarelabs <hello@lumarelabs.com>
2025-06-20 00:59:35 +0300

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,8 @@ import { useFonts, Inter_400Regular, Inter_500Medium, Inter_700Bold } from '@exp
 import * as SplashScreen from 'expo-splash-screen';
 import { AppProvider } from '@/app/context/AppContext';
 import { useAuth } from '@/app/hooks/useAuth';
-import AuthScreen from '@/app/components/auth/AuthScreen';
+import AuthScreen from '@/app/components/auth/AuthScreen'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 // Prevent splash screen from auto-hiding
 SplashScreen.preventAutoHideAsync();
